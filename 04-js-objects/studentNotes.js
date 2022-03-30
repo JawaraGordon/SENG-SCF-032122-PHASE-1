2057920da736031452add80dca8ca3931440df81
JawaraGordon <jawaragordon@icloud.com>
2022-03-30 15:09:14 -0400

    033122 objects

--- 04-js-objects/studentNotes.js
+++ 04-js-objects/studentNotes.js
@@ -5,3 +5,18 @@
 // Running the file ```node notes.js``` (without the backticks) will allow you to explore/play around with code. Make sure you have cd'ed into the inner folder before you do this.
 
 console.log("hi there!!")
+
+
+const pokemon = {name: "Pikachu", img: "https://imageofPikachu.com", likes: 0};
+
+function increaseValue(obj,key){
+    return ++obj[key]
+};
+
+increaseValue(pokemon,"likes")
+
+
+/* --create a new complex object that fills in data to represent you, and practice accessing various properties manually. Then write functions to try to access those same values: */
+
+// for...in loop
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
